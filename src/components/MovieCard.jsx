f1ebf6b40006bed7e66349c0fea2be2d78dc320c
Arif Hussain Rahimi <arifhussain.rahimi84@gmail.com>
2025-01-30 11:33:31 +0430

    featch data from API and use in HOme Page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ const MovieCard = ({movie})=> {
     }
     return <div className="movie-card">
         <div className="movie-poster">
-            <img src={movie.url} alt={movie.title} />
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
             <div className="movie-overlay">
                 <button className="favorite-btn"
                     onClick={onFavoriteClick}
@@ -15,6 +15,7 @@ const MovieCard = ({movie})=> {
                 </button>
             </div>
         </div>
+        
         <div className="movie-info">
             <h3>{movie.title}</h3>
             <p>{movie.release_date}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,76 @@
-import { useState } from 'react';
-import MovieCard from './../components/MovieCard';
+import { useEffect, useState } from "react";
+import MovieCard from "./../components/MovieCard";
 import "../css/Home.css";
+
+import { searchMovies, getPopularMovies } from "../services/api";
+
+
 const Home = () => {
-    const [search, setSearch] = useState();
-    const movies = [
-        { id: 1, title: "Johan Wick", release_date: "2020" },
-        { id: 2, title: "Terminatro", release_date: "2020" },
-        { id: 3, title: "The Matrix", release_date: "2020" },
-        { id: 4, title: "Johan Wick", release_date: "2020" },
-        { id: 5, title: "Johan Wick", release_date: "2020" },
-        { id: 6, title: "Johan Wick", release_date: "2020" },
-    ]
+  const [search, setSearch] = useState();
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (error) {
+        console.log("failed to load movies from ALI ", error);
+        setError("Failed to load movies.... ");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
 
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if(!search.trim()) return 
+    if(loading) return
 
-    const handleSearch = (e) => {
-        e.preventDefault();
-        alert("the serach button", search)
-        setSearch(".............")
+    setLoading(true);
+    try {
+        const searchResults = await searchMovies(search)
+        setMovies(searchResults)
+        setError(null)
+    } catch (error) {
+        console.log(error)
+        setError("Failed to search movies...")
+
+    }finally{
+        setLoading(false)
     }
+  };
 
-
-    return (
-        <div className="home">
-            <form onSubmit={handleSearch} className="search-form">
-                <input type='text'
-                    placeholder='search for the movies..'
-                    className='search-input'
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-                <button
-                    type="submit"
-                    className='search-button'
-                >Search
-                </button>
-            </form>
-            <div className="movies-grid">
-                {movies.map(
-
-                    // movie => movie.t
-                    movie=>
-                    <MovieCard movie={movie} key={movie.id} />
-                    // )
-                )}
-                    
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="search for the movies.."
+          className="search-input"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      {error&&<div className="error-message">{error}</div>}
+      {loading? <div className="loading">loading...</div>:<div className="movies-grid">
+        {movies.map(
+          // movie => movie.t
+          (movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          )
+          // )
+        )}
+      </div>}
+      
+    </div>
+  );
+};
 
 export default Home;
